7c92b3e1f6744755d80fe22d7b9be557b335ec0c
Auctioneer <ljgdavie@hotmail.com>
2016-04-08 20:09:49 +0100

    Added a platform destroyer to remove platforms that can no longer be seen above the canvas.

---
+++ SpyFall/Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformDestroyer : MonoBehaviour {
+
+	//Destroys every object it touches
+	void OnTriggerEnter2D(Collider2D enterObject)
+	{
+		//If statement there just in case any of the UI elements overlap above the canvas
+		if ((enterObject.tag != "Default") || (enterObject.tag != "UI")) 
+		{
+			Destroy (enterObject.gameObject);
+		}
+	}
+}
