4a214829fe7ae84a472335e21dd454ad3507e6b5
Auctioneer <ljgdavie@hotmail.com>
2016-04-08 21:20:05 +0100

    Platform generator now works. Platform position and speed remain constant - this will have to be changed. Note: the generator script component allows passing different prefabs in, so this can be used to generate one of a few choices of platform.

---
+++ SpyFall/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject[] obj;
+
+	//The gap in seconds between spawning objects
+	public float spawnMin = 1f;
+	public float spawnMax = 2f;
+
+
+	// Use this for initialization
+	void Start () {
+
+		//Call generate the first time to kick things off
+		Generate ();
+	}
+
+
+	//Method to generate platforms
+	void Generate()
+	{
+		//Create an object?
+		Instantiate(obj[Random.Range (0, obj.Length)], transform.position, Quaternion.identity);
+
+		//Method calls itself again after the interval determined by min and max
+		Invoke("Generate", Random.Range(spawnMin, spawnMax));
+	}
+}
