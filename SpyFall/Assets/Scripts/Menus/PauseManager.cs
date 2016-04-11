ee14b0d76745da6cd336ebb868991cfc84bcda71
Auctioneer <ljgdavie@hotmail.com>
2016-04-11 15:43:25 +0100

    More work on Pause menu. Continue button doesn't work but can pause with escape.

--- SpyFall/Assets/Scripts/Menus/PauseManager.cs
+++ SpyFall/Assets/Scripts/Menus/PauseManager.cs
@@ -1,15 +1,64 @@
 ﻿using UnityEngine;
+//For sliders, buttons, text, etc. If we need them.
+using UnityEngine.UI;
 using System.Collections;
 
 public class PauseManager : MonoBehaviour {
 
+	//Pause panel
+	public GameObject panel;
+
+	//Whether the game is paused or not
+	public bool paused;
+
+
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		//Game starts unpaused
+		paused = false;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+	{
+		if (paused == true) 
+		{
+			Pause (true);
+		}
+		else
+		{
+			Pause (false);
+		}
+
+		if (Input.GetButtonDown ("Cancel")) 
+		{
+			ChangePauseState ();
+		}
+	}
+
+	void Pause(bool state)
+	{
+		if (state == true) 
+		{
+			//Make panel appear
+			panel.SetActive (true);
+
+			//Stop time
+			Time.timeScale = 0.0f;
+		}
+		else 
+		{
+			//Hide panel and resume time
+			panel.SetActive (false);
+			Time.timeScale = 1.0f;
+		}
+	}
+
+	//Flip pause state
+	public void ChangePauseState()
+	{
+		paused = !paused;
 	}
 }
