0486bd0eef505d6d164c799b7ee60308f3a9bb20
Auctioneer <ljgdavie@hotmail.com>
2016-04-14 17:09:16 +0100

    Reworked bumpers to fit screen size. Resolution stuff may be an issue, fair warning...

---
+++ SpyFall/Assets/Scripts/BumperScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BumperScript : MonoBehaviour {
+
+	//X and Y co-ordinates to change in inspector
+	//0,0 is bottom left of camera view
+	public int xCo = 0;
+	public int yCo = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Vector3 location;
+
+		if (this.gameObject.name == "LeftBumper")
+		{
+			location = Camera.main.ScreenToWorldPoint (new Vector3 (xCo, yCo, 5));
+		}
+		else
+		{
+			location = Camera.main.ScreenToWorldPoint (new Vector3 (Screen.width + xCo, yCo, 5));
+		}
+		this.transform.position = location;
+	}
+
+}
--- SpyFall/Assets/Scripts/PlatformDestroyer.cs
+++ SpyFall/Assets/Scripts/PlatformDestroyer.cs
@@ -7,7 +7,7 @@ public class PlatformDestroyer : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D enterObject)
 	{
 		//If statement there just in case any of the UI elements overlap above the canvas
-		if ((enterObject.tag != "Default") && (enterObject.tag != "UI") && (enterObject.tag != "Player")) 
+		if ((enterObject.tag != "Default") && (enterObject.tag != "UI") && (enterObject.tag != "Player") && (enterObject.tag != "Bumper")) 
 		{
 			Destroy (enterObject.gameObject);
 		}
