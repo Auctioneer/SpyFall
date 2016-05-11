d07e5deb1d3f85ae96fc7e613acc9224ae14bd25
Auctioneer <ljgdavie@hotmail.com>
2016-05-11 13:11:11 +0100

    Fixed timer bug.

--- SpyFall/Assets/Scripts/TimerScript.cs
+++ SpyFall/Assets/Scripts/TimerScript.cs
@@ -17,12 +17,24 @@ public class TimerScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-
+		//If the timer isn't 0, count down
+		if (!timerLength.ToString("0").Equals("0"))
+		{
 			//Timer needs to count down every second in real time
 			//This code maps it to real time
 			timerLength -= Time.deltaTime;
 
 			//Format this into a string - no decimals
-			timerText.text = timerLength.ToString("0");
+			timerText.text = timerLength.ToString ("0");
+		}
+		else
+		{
+			//Stop the timer and disable the script (this leaves the UI timer at 0)
+			timerLength = 0.0f;
+			this.enabled = false;
+
+			//Either here or before the line above, we tell the game manager the time has run out
+		}
+
 	}
 }
