7485dc91d835e5470de5f5f2016bbdb1ffb13c2b
Auctioneer <ljgdavie@hotmail.com>
2016-05-16 18:56:39 +0100

    Working on main menu.

--- SpyFall/Assets/Scripts/Menus/openingMenuScript.cs
+++ SpyFall/Assets/Scripts/Menus/openingMenuScript.cs
@@ -9,11 +9,12 @@ public class openingMenuScript : MonoBehaviour {
 	public Text spyFallText;
 	public Button playText;
 	public Button exitText;
+	public Button controlsText;
 
 	//Canvases
 	public Canvas startMenu;
 	public Canvas quitMenu;
-
+	public Canvas controls;
 
 	//Initialise
 	void Start () 
@@ -21,9 +22,13 @@ public class openingMenuScript : MonoBehaviour {
 		//Get components from hierarchy with respective names
 		startMenu = startMenu.GetComponent<Canvas> ();
 		quitMenu = quitMenu.GetComponent<Canvas> ();
+		controls = controls.GetComponent<Canvas> ();
 
 		//Quit should not be visible until the quit button is pressed
 		quitMenu.enabled = false;
+
+		//Neither should the contols window
+		controls.enabled = false;
 
 		spyFallText = spyFallText.GetComponent<Text> ();
 		playText = playText.GetComponent<Button> ();
@@ -36,8 +41,7 @@ public class openingMenuScript : MonoBehaviour {
 	{
 		//Show menu
 		quitMenu.enabled = true;
-		playText.enabled = false;
-		exitText.enabled = false;
+		DisableMainMenu ();
 	}
 
 	//User clicks no in quit menu
@@ -45,8 +49,7 @@ public class openingMenuScript : MonoBehaviour {
 	{
 		//Go back to main menu view
 		quitMenu.enabled = false;
-		playText.enabled = true;
-		exitText.enabled = true;
+		EnableMainMenu ();
 	}
 
 	//Load the fight
@@ -64,5 +67,30 @@ public class openingMenuScript : MonoBehaviour {
 		Application.Quit ();
 	}
 
+	//Show control scheme
+	public void ViewControls ()
+	{
+		controls.enabled = true;
+		DisableMainMenu ();
+	}
+
+	//Back button within view controls to go back to main menu
+	public void HideControls()
+	{
+	}
+
+	void DisableMainMenu()
+	{
+		playText.enabled = false;
+		exitText.enabled = false;
+		controlsText.enabled = false;
+	}
+
+	void EnableMainMenu()
+	{
+		playText.enabled = true;
+		exitText.enabled = true;
+		controlsText.enabled = true;
+	}
 
 }
