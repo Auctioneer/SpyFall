f2a83b3806d7e9af49199d6f8c6ac1553ba37961
Auctioneer <ljgdavie@hotmail.com>
2016-05-17 21:43:07 +0100

    More initialising stuff. Need to get countdown timer to activate BegintheBegin()

--- SpyFall/Assets/Scripts/StartManager.cs
+++ SpyFall/Assets/Scripts/StartManager.cs
@@ -8,20 +8,35 @@ public class StartManager : MonoBehaviour {
 
 	public Canvas UICanvas;
 
+	//Left and right initial platforms
 	public GameObject platformOne;
 	public GameObject platformTwo;
 
+	public GameObject platformGenerators;
 
+	public GameObject playerDestroyer;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+		playerDestroyer.SetActive (false);
+		platformGenerators.SetActive (false);
+		BeginTheBegin ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () 
 	{
 		
+	}
+
+	//Life's rich demand creates supply in the hand of the power
+	void BeginTheBegin()
+	{
+		//Enable all the things in the game
+		playerDestroyer.SetActive (true);
+		platformGenerators.SetActive (true);
+		platformOne.GetComponent<MovingPlatform> ().enabled = true;
+		platformTwo.GetComponent<MovingPlatform> ().enabled = true;
 	}
 }
